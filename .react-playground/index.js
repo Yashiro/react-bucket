d414ff9d47d7e40a2469860c94200c73392c7481
Andy <andy@AndydeMacBook-Air.local>
2017-09-10 01:46:38 +0800

    chang dependencies

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from '/Users/andy/vscode/react-bucket/src/index.js';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
