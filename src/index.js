f093b45672f65260b3ccc583554020149f7e2b7f
Yashiro <zeilta@126.com>
2017-09-14 20:55:41 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import UserAddPage from './pages/UserAdd';
 import HomePage from './pages/Home';
-import UserList from './pages/UserList';
+import UserListPage from './pages/UserList';
+import UserAddPage from './pages/UserAdd';
 
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={HomePage} />
+        <Route path="/user/list" component={UserListPageList} />
         <Route path="/user/add" component={UserAddPage} />
-        <Route path="/user/list" component={UserList} />
     </Router>
 ), document.getElementById('app'));
