18c1c180bd0b7ba4ee3f19c72058b88b60d48733
Yashiro <zeilta@126.com>
2017-09-14 20:59:34 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import UserAddPage from './pages/UserAdd';
 import UserListPage from './pages/UserList';
+import UserAddPage from './pages/UserAdd';
 import HomePage from './pages/Home';
 
 ReactDOM.render((
