a6b24e47aa067a6ad09a846023995b0a5632cba1
Yashiro <zeilta@126.com>
2017-09-14 20:59:55 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import HomePage from './pages/Home';
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={HomePage} />
-        <Route path="/user/add" component={UserAddPage} />
         <Route path="/user/list" component={UserListPage} />
+        <Route path="/user/add" component={UserAddPage} />
     </Router>
 ), document.getElementById('app'));
