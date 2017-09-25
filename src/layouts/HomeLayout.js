8c354c2d64f03b4088a79820097b4472ded12df3
Yashiro <zeilta@126.com>
2017-09-26 01:23:29 +0800

    change HomeLayout Style

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,47 +1,71 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Menu, Icon } from 'antd';
-import style from '../styles/home-layout.less';
+import { Menu, Icon, Layout } from 'antd';
 
 const SubMenu = Menu.SubMenu;
 const MenuItem = Menu.Item;
+const { Header, Content, Footer, Sider } = Layout;
 class HomeLayout extends Component {
+    state = {
+        collapsed: false,
+        mode: 'inline',
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
     render() {
-        const { children } = this.props;
+        const { title, children } = this.props;
         return (
-            <div>
-                <header className={style.header}>
-                    <Link to="/">ReactManager</Link>
-                </header>
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <Header style={{ background: '#404040', padding: 0 }}>
+                    </Header>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <SubMenu key="user" title={<span><Icon type="user" /><span>用户管理</span></span>}>
+                            <MenuItem key="user-list">
+                                <Link to="/user/list">用户列表</Link>
+                            </MenuItem>
+                            <MenuItem key="user-add">
+                                <Link to="/user/add">添加用户</Link>
+                            </MenuItem>
+                        </SubMenu>
 
-                <main className={style.main}>
-                    <div className={style.menu}>
-                        <Menu mode="inline" theme="dark" style={{ width: '240px' }}>
-                            <SubMenu key="user" title={<span><Icon type="user" /><span>用户管理</span></span>}>
-                                <MenuItem key="user-list">
-                                    <Link to="/user/list">用户列表</Link>
-                                </MenuItem>
-                                <MenuItem key="user-add">
-                                    <Link to="/user/add">添加用户</Link>
-                                </MenuItem>
-                            </SubMenu>
-
-                            <SubMenu key="book" title={<span><Icon type="book" /><span>图书管理</span></span>}>
-                                <MenuItem key="book-list">
-                                    <Link to="/book/list">图书列表</Link>
-                                </MenuItem>
-                                <MenuItem key="book-add">
-                                    <Link to="/book/add">添加图书</Link>
-                                </MenuItem>
-                            </SubMenu>
-                        </Menu>
-                    </div>
-
-                    <div className={style.content}>
-                        {children}
-                    </div>
-                </main>
-            </div>
+                        <SubMenu key="book" title={<span><Icon type="book" /><span>图书管理</span></span>}>
+                            <MenuItem key="book-list">
+                                <Link to="/book/list">图书列表</Link>
+                            </MenuItem>
+                            <MenuItem key="book-add">
+                                <Link to="/book/add">添加图书</Link>
+                            </MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#404040', padding: 0 }}>
+                        <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                                style={{cursor: 'pointer'}}
+                            />
+                        </span>
+                        <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>Information Management System</span>
+                    </Header>
+                    <Content style={{ margin: '0 16px' }}>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 780 }}>
+                            {children}
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        ©2017 Andy
+                    </Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
