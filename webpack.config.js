da83fdac943a97630a8ac74f76b33465117f7bbb
Yashiro <zeilta@126.com>
2017-09-28 15:36:51 +0800

    modify sources map

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = function (webpackConfig, env) {
-  webpackConfig.devtool = 'source-map';
   webpackConfig.output.devtoolModuleFilenameTemplate = info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/');
 
   if (env === 'development') {
