6a44f6fb7e83b1b633bcc3dc8dccabc88bcdd33b
Yashiro <zeilta@126.com>
2017-09-28 20:07:02 +0800

    modify package conf & webpack less loader conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = function (webpackConfig, env) {
   webpackConfig.output.devtoolModuleFilenameTemplate = info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/');
+
+  webpackConfig.module.rules.forEach(function(loader, index) {
+    if (index >= 2) {
+      if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
+        loader.include = /node_modules/;
+        loader.test = /\.less$/;
+      }
+      if (loader.test.toString() === '/\\.module\\.less$/') {
+        loader.exclude = /node_modules/;
+        loader.test = /\.less$/;
+      }
+      if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.css$') > -1) {
+        loader.include = /node_modules/;
+        loader.test = /\.css$/;
+      }
+      if (loader.test.toString() === '/\\.module\\.css$/') {
+        loader.exclude = /node_modules/;
+        loader.test = /\.css$/;
+      }
+    }
+  });
 
   if (env === 'development') {
     webpackConfig.resolve.alias = {
