1285613c341c1160f199f36f4293ff819aa8db90
Yashiro <zeilta@126.com>
2017-09-28 20:30:07 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = function (webpackConfig, env) {
   webpackConfig.output.devtoolModuleFilenameTemplate = info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/');
 
   webpackConfig.module.rules.forEach(function(loader, index) {
     if (index >= 2) {
+      console.error(loader.use);
+      return;
       if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
         loader.include = /node_modules/;
         loader.test = /\.less$/;
+        loader.use.require.resolve('style-loader');
+        loader.use.require.resolve('css-loader');
       }
       if (loader.test.toString() === '/\\.module\\.less$/') {
         loader.exclude = /node_modules/;
@@ -17,6 +20,7 @@ module.exports = function (webpackConfig, env) {
       if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.css$') > -1) {
         loader.include = /node_modules/;
         loader.test = /\.css$/;
+        loader.use.require.resolve('style-loader');
       }
       if (loader.test.toString() === '/\\.module\\.css$/') {
         loader.exclude = /node_modules/;
